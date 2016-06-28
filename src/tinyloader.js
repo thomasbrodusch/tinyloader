1be88ad625a8613ba2c7eb9a7ab6d0adade8710f
Thomas B <tom4dev@users.noreply.github.com>
2016-06-28 12:41:30 +0200

    Version 1.1.0 prePublish

--- src/tinyloader.js
+++ src/tinyloader.js
@@ -2,7 +2,7 @@
  * TinyloaderJS
  * author: Thomas Brodusch
  * license: MIT
- * version: 1.0.0
+ * version: 1.1.0
  * ##########################
  * Script loader using async and ES6 Promises 
  * @param  string or Array  url     Can be a string or en array of URLs (of course you can chain your script loading)
@@ -13,7 +13,7 @@ import CacheManager from './cachemanager';
 const Cache = new CacheManager();
 const msg_header = 'TinyloadJS | ';
 
-const load = (url, mode = 'defer', callback = false) => {
+const load = (url, mode = 'async') => {
 
   if(Array.isArray(url)) {
     let promises = [];
@@ -21,18 +21,12 @@ const load = (url, mode = 'defer', callback = false) => {
       // Recursivity for an array of URLs
       promises.push(load(item));
     });
-    return Promise.all(promises)
-          .then(() => {
-            successLoad(url, callback);
-          })
-          .catch((e) => {
-            failedLoad(url, e);
-          });
+    return Promise.all(promises);
   }
 
   return new Promise((resolve, reject) => {
     //Check if the script is already loaded (present in cache)
-    if(Cache.getFromCache('tinyload', url) != true){
+    if(Cache.getFromCache('tinyload', url) !== true){
       Cache.addToCache('tinyload', url, 'loading');
       // Load the script.
       let r = false,
@@ -43,45 +37,43 @@ const load = (url, mode = 'defer', callback = false) => {
       s.src = url;
       s.async = mode === 'async' ? true : false;
       s.defer = mode === 'defer' ? true: false;
+      
       s.onload = s.onreadystatechange = function () {
         if (!r && (!this.readyState || this.readyState == 'complete') ) {
           r = true;
+          successLoad(url);
           resolve(this)
         }
       };
-      s.onerror = s.onabort = reject;
+      
+      s.onerror = s.onabort = (e) => {
+        reject(failedLoad(url, e));
+      };
+
       t.parentNode.insertBefore(s, t);
     }
     else{
-      throw 'already_load';
-    }
-  })
-  .then(() => {
-    successLoad(url, callback);
-  })
-  .catch((e) => {
-    if(e === 'already_load'){
-      alreadyLoad(url, callback);
-    } else {
-      failedLoad(url, e);
+      alreadyLoad(url);  
+      resolve(this);
     }
   });
 };
 
-const successLoad = (url, callback) => {
-  Cache.addToCache('tinyload', url, true);
-  return callback ? callback() : true;
+const successLoad = (url) => {
+  return Cache.addToCache('tinyload', url, true);
 };
 
-const alreadyLoad = (url, callback) => {
+const alreadyLoad = (url) => {
   console.info(`${msg_header} Script "${url}" is already loaded.`);  
-  return callback ? callback() : true;
 };
 
 const failedLoad = (url, e) => {
   console.error(`${msg_header} An error has occured when trying to load the script: "${url}", please check if the path is correct.`);
-  console.error(`${msg_header} Event error: `);
-  return console.error(e);
+  console.error(`${msg_header} Reason: `);
+  return {
+    script: url, 
+    error: e
+  };
 }
 
 export{
